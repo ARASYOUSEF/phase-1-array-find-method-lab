14bf79fceba631cfc6faf7ccd32ee3d9efb19caa
ARASYOUSEF <aras.yousef.a@gmail.com>
2024-03-02 19:10:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    {year: "2024", result: "L"},
+    {year: "2023", result: "W"},
+    {year: "2022", result: "N/A"}
+ 
+  ]
+function superbowlWin(array){
+   const old = array.find(age => age.result === "W")
+   if (old){
+    return old.year
+   } else {
+    return undefined
+   }
+}
